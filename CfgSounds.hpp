9c862fec9b30abdfbb23c211b235d01aa708a566
b-mayr-1984 <b.mayr.1984@googlemail.com>
2026-03-08 16:38:28 +0100

    added ATC radio traffic

--- CfgSounds.hpp
+++ CfgSounds.hpp
@@ -1,5 +1,6 @@
 class CfgSounds
 {
+	// morse signals
 	class morse_package_number_1
 	{
 		name = "morse_package_number_1";
@@ -25,6 +26,118 @@ class CfgSounds
 	{
 		name = "morse_package_number_4";
 		sound[] = {"sounds\morse_package_number_4.ogg", db+12, 1};
+		titles[] = {};
+	};
+
+	// Cessna radio calls
+	class Cessna_request_takeoff_Bala
+	{
+		name = "Cessna_request_takeoff_Bala";
+		sound[] = {"sounds\Bala Tower, this is Cessna 1 3. Request permission for takeoff.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_landing_Bala
+	{
+		name = "Cessna_request_landing_Bala";
+		sound[] = {"sounds\Bala Tower, this is Cessna 1 3. Request permission to land.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_takeoff_La_Rochelle
+	{
+		name = "Cessna_request_takeoff_La_Rochelle";
+		sound[] = {"sounds\La Rochelle Tower, this is Cessna 1 3. Request permission for takeoff.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_landing_La_Rochelle
+	{
+		name = "Cessna_request_landing_La_Rochelle";
+		sound[] = {"sounds\La Rochelle Tower, this is Cessna 1 3. Request permission to land.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_takeoff_Saint_George
+	{
+		name = "Cessna_request_takeoff_Saint_George";
+		sound[] = {"sounds\Saint-George Tower, this is Cessna 1 3. Request permission for takeoff.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_landing_Saint_George
+	{
+		name = "Cessna_request_landing_Saint_George";
+		sound[] = {"sounds\Saint-George Tower, this is Cessna 1 3. Request permission to land.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_takeoff_Tuvanaka
+	{
+		name = "Cessna_request_takeoff_Tuvanaka";
+		sound[] = {"sounds\Tuvanaka Tower, this is Cessna 1 3. Request permission for takeoff.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_request_landing_Tuvanaka
+	{
+		name = "Cessna_request_landing_Tuvanaka";
+		sound[] = {"sounds\Tuvanaka Tower, this is Cessna 1 3. Request permission to land.wav", db+12, 1};
+		titles[] = {};
+	};
+	class Cessna_more_landing_practice
+	{
+		name = "Cessna_more_landing_practice";
+		sound[] = {"sounds\Tanoa Towers, this is Cessna 1 3. We'll go for another round of landing practice.wav", db+12, 1};
+		titles[] = {};
+	};
+
+	// Bala tower radio calls
+	class Bala_landing_approved
+	{
+		name = "Bala_landing_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for landing (Aria).wav", db+12, 1};
+		titles[] = {};
+	};
+	class Bala_takeoff_approved
+	{
+		name = "Bala_takeoff_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for takeoff (Aria).wav", db+12, 1};
+		titles[] = {};
+	};
+
+	// La Rochelle tower radio calls
+	class La_Rochelle_landing_approved
+	{
+		name = "La_Rochelle_landing_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for landing (Eric).wav", db+12, 1};
+		titles[] = {};
+	};
+	class La_Rochelle_takeoff_approved
+	{
+		name = "La_Rochelle_takeoff_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for takeoff (Eric).wav", db+12, 1};
+		titles[] = {};
+	};
+
+	// Saint-George tower radio calls
+	class Saint_George_landing_approved
+	{
+		name = "Saint_George_landing_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for landing (Ava).wav", db+12, 1};
+		titles[] = {};
+	};
+	class Saint_George_takeoff_approved
+	{
+		name = "Saint_George_takeoff_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for takeoff (Ava).wav", db+12, 1};
+		titles[] = {};
+	};
+
+	// Tuvanaka tower radio calls
+	class Tuvanaka_landing_approved
+	{
+		name = "Tuvanaka_landing_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for landing (Roger).wav", db+12, 1};
+		titles[] = {};
+	};
+	class Tuvanaka_takeoff_approved
+	{
+		name = "Tuvanaka_takeoff_approved";
+		sound[] = {"sounds\Cessna 1 3 cleared for takeoff (Roger).wav", db+12, 1};
 		titles[] = {};
 	};
 };
